a9494f268ae0991bdf3343b585096c797652758c
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-06-12 20:37:18 +0300

    modified user model to add error message and email domain validation

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -3,40 +3,40 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
-    firstName: {
-      type: String,
-      minlength: 3,
-      maxlength: 8,
-      required: [true, 'Please add Your firstName'],
-    },
-    lastName: {
-      type: String,
-      minlength: 3,
-      maxlength: 8,
-      required: [true, 'Please add Your lastName'],
-    },
-    username: {
-      type: String,
-      minlength: 3,
-      maxlength: 8,
-      required: [true, 'Please add a username'],
-      unique: [true, 'username is already used'],
-    },
-    email: {
-      type: String,
-      required: [true, 'Please add an email'],
-      unique: [true, 'email is already used'],
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        'Please add a valid email',
-      ],
-    },
-    password: {
-      type: String,
-      required: [true, 'Please add a password'],
-      minlength: 8,
-      select: false,
-    },
+  firstName: {
+    type: String,
+    minlength: [3, 'First name must be at least 3 characters'],
+    maxlength: [8, 'First name can be at most 8 characters'],
+    required: [true, 'Please add your first name'],
+  },
+  lastName: {
+    type: String,
+    minlength: [3, 'Last name must be at least 3 characters'],
+    maxlength: [8, 'Last name can be at most 8 characters'],
+    required: [true, 'Please add your last name'],
+  },
+  username: {
+    type: String,
+    minlength: [3, 'Username must be at least 3 characters'],
+    maxlength: [8, 'Username can be at most 8 characters'],
+    required: [true, 'Please add a username'],
+    unique: [true, 'Username is already taken'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: [true, 'Email is already taken'],
+    match: [
+      /^[\w.+-]+@(gmail|yahoo|hotmail)\.com$/,
+      'Please add a valid email with @gmail, @yahoo, or @hotmail domain',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: [8, 'Password must be at least 8 characters'],
+    select: false,
+  },
 }, { timestamps: true });
 
 UserSchema.pre('save', async function (next) {
