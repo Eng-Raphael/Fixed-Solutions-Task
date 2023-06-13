c93d89af74e8fb489f0c4d3b0317a3f729306b79
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-06-13 06:55:25 +0300

    adding vedio schema

---
+++ Backend/models/Vedio.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const vedioSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please enter title'],
+        trim: true
+    },
+    description:{
+        type: String,
+        required: [true, 'Please enter description'],
+    },
+    photographer:{
+        type: String,
+        required: [true, 'Please enter photographer'],
+    },
+    nasa_id:{
+        type: String,
+        required: [true, 'Please enter nasa_id'],
+    },
+    url:{
+        type: String,
+        required: [true, 'Please enter url'],
+    },
+    media_type:{
+        type: String,
+        required: [true, 'Please enter media_type'],
+    }
+});
+
+module.exports = mongoose.model('Vedio', vedioSchema);
